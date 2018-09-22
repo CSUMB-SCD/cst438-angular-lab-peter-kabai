8ce249963d0014d3771d2d22f3d14bd3c25216dc
Peter <pkabai@csumb.edu>
2018-09-21 22:39:23 -0700

    fixes url

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -13,9 +13,9 @@ export class DetailsComponent implements OnInit {
   user$: Object;
 
   constructor(private data: DataService, private route: ActivatedRoute) { 
-    this.route.params.subscribe(
-      params => this.user$ = params.id
-    )
+    //this.route.params.subscribe(
+    //  params => this.user$ = params.id
+    //)
   }
 
   ngOnInit() {
